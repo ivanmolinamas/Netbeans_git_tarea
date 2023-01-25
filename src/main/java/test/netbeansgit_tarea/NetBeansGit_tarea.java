e45862572a42f42efc0e14bb577dbc5b9c809a13
ivan Molina <ivanmolinamas@gmail.com>
2023-01-25 11:08:38 +0100

    Primer commit para añadir archivos

---
+++ src/main/java/test/netbeansgit_tarea/NetBeansGit_tarea.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package test.netbeansgit_tarea;
+
+/**
+ *
+ * @author IVAN
+ */
+public class NetBeansGit_tarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Ivan Molina Mas");
+    }
+}
